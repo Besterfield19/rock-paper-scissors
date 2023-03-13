2be83deb413a8321af2680e397f22025e21abee3
Bea <115370186+Besterfield19@users.noreply.github.com>
2023-03-13 16:36:17 -0500

    Remove the logic that plays exactly five rounds

--- html.js
+++ html.js
@@ -1,30 +1,30 @@
 
 function getComputerChoice() {
-     let choice = ["Rock", "Paper", "Scissors"];
+     let choice = ["rock", "paper", "scissors"];
      let randomRPS = choice[Math.floor(Math.random() * choice.length)];
      return randomRPS;
 }
 
 function playRound(playerSelection, computerSelection) {
-     if (playerSelection == "Rock" && computerSelection == "Paper") {
-          return "You lose! Rock beats Paper.  The robot uprising will now commence."
-     } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-          return "You win! Paper beats Rock! There may still be hope for humanity!";
-     } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-          return "You win!  Rock beats Scissors!  Rest well knowing that humanity is safe.";
-     } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-          return "You lose!  Rock beats Scissors!  The robot uprising will now commence.";
-     } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-          return "You lose!  Scissors beats Paper!  The robot uprising will now commence.";
-     } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-          return "You win!  Scissors beats Paper!  Report to the office for your prize.";
+     if (playerSelection == "rock" && computerSelection == "paper") {
+          return "The Computer threw: Paper. You lose! Rock beats Paper.  The robot uprising will now commence."
+     } else if (playerSelection == "paper" && computerSelection == "rock") {
+          return "The Computer threw: Rock.  You win! Paper beats Rock! There may still be hope for humanity!";
+     } else if (playerSelection == "rock" && computerSelection == "scissors") {
+          return "The Computer threw: Scissors.  You win!  Rock beats Scissors!  Rest well knowing that humanity is safe.";
+     } else if (playerSelection == "scissors" && computerSelection == "rock") {
+          return "The Computer threw: Rock. You lose!  Rock beats Scissors!  The robot uprising will now commence.";
+     } else if (playerSelection == "paper" && computerSelection == "scissors") {
+          return "The Computer threw: Scissors.  You lose!  Scissors beats Paper!  The robot uprising will now commence.";
+     } else if (playerSelection == "scissors" && computerSelection == "paper") {
+          return "The Computer threw: Paper.  You win!  Scissors beats Paper!  Report to the office for your prize.";
      } else if (playerSelection === computerSelection) {
           return "It seems that we have reached an impasse.";
      }
 }
 
 
-function game() {
+/*function game() {
      for (let i = 0; i < 5; i++) {
           let playerSelection = prompt(
                "Pick your fighter!"
@@ -34,4 +34,4 @@ function game() {
      }
 }
 
-game();
+game();*/
